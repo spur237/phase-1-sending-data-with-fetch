5b55d4142c3c8eba2d69d79ccb1ef7c905537b76
spur237 <samuel.nzioka@student.moringaschool.com>
2022-09-01 16:03:41 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+function submitData(name, email) {
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const userInput = document.getElementById("searchByID");
+  
+      fetch(`http://localhost:3000/movies/${userInput.value}`)
+      .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        });
+    })
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
+  
+  function submitData(name, email){
+      let contentOn = {
+          method: "POST", 
+          headers: {
+              "Content-Type": "application/json", 
+              "Accept": "application/json"
+          }, 
+          body: JSON.stringify({name, email})
+      }
+      return fetch("http://localhost:3000/users", contentOn)
+      .then((resp) => resp.json())
+      .then(obj => {
+          document.body.innerHTML = obj.id
+  
+      }).catch((err) => {
+          document.body.innerHTML = err.message
+      })
+  }
